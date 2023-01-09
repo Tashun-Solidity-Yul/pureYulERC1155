2f79b5531569bddb871e75d61e3341fd6ef978bf
tashun <tashun.chamara@gmail.com>
2023-01-09 23:55:49 +0530

    safe transfer tests

--- test/contract-testing/safeTransferFrom.ts
+++ test/contract-testing/safeTransferFrom.ts
@@ -16,10 +16,10 @@ export function readJson(path: string) {
     }
 }
 
-describe("Pure Yul ERC1155 - mintBatch Function Testing", function () {
+describe("Pure Yul ERC1155 - safeTransferFrom Function Testing", function () {
     let deployer, user1: any, user2: any;
     let contract: Contract;
-    describe("mintBatch Values - Case 01", async () => {
+    describe("Transfer from - Case 01", async () => {
 
         before(async () => {
             [deployer, user1, user2] = await ethers.getSigners();
@@ -35,38 +35,44 @@ describe("Pure Yul ERC1155 - mintBatch Function Testing", function () {
 
         })
         it("Owner Transfer from user2 to user1", async () => {
-            await contract.connect(user2).safeTransferFrom(user2.address, user1.address, 3, 100, 0x00)
-            expect(await contract.balanceOf(user2.address, 3), "spender balance updated successfully").to.be.equal(9900)
-            expect(await contract.balanceOf(user1.address, 3), "receiver balance updated successfully").to.be.equal(200)
+            const tnx1 = await contract.connect(user2).safeTransferFrom(user2.address, user1.address, 3, 100, 0x00)
+            const receipt = await tnx1.wait()
+            //todo
+            receipt.events.forEach((a: { topics: any, data: any;  }) => {
+                console.log(a.topics)
+                console.log(a.data)
+            })
+            expect(await contract.balanceOf(user2.address, 3), "spender balance updated failed").to.be.equal(9900)
+            expect(await contract.balanceOf(user1.address, 3), "receiver balance updated failed").to.be.equal(200)
 
         })
 
         it("Owner Transfer from user1 to user2", async () => {
             await contract.connect(user1).safeTransferFrom(user1.address, user2.address, 4, 100, 0x00)
-            expect(await contract.balanceOf(user1.address, 4), "spender balance updated successfully").to.be.equal(9900)
-            expect(await contract.balanceOf(user2.address, 4), "receiver balance updated successfully").to.be.equal(200)
+            expect(await contract.balanceOf(user1.address, 4), "spender balance updated failed").to.be.equal(9900)
+            expect(await contract.balanceOf(user2.address, 4), "receiver balance updated failed").to.be.equal(200)
 
         })
         it("Transfer From with user2 set user1 the approve all", async () => {
             await contract.connect(user2).setApprovalForAll(user1.address, true)
             await contract.safeTransferFrom(user2.address, user1.address, 1, 100, 0x00)
-            expect(await contract.balanceOf(user2.address, 1), "spender balance updated successfully").to.be.equal(9900)
-            expect(await contract.balanceOf(user1.address, 1), "receiver balance updated successfully").to.be.equal(200)
+            expect(await contract.balanceOf(user2.address, 1), "spender balance updated failed").to.be.equal(9900)
+            expect(await contract.balanceOf(user1.address, 1), "receiver balance updated failed").to.be.equal(200)
 
         })
 
         it("Transfer From with user1 set user2 the approve all", async () => {
             await contract.connect(user1).setApprovalForAll(user2.address, true)
             await contract.safeTransferFrom(user1.address, user2.address, 2, 100, 0x00)
-            expect(await contract.balanceOf(user1.address, 2), "spender balance updated successfully").to.be.equal(9900)
-            expect(await contract.balanceOf(user2.address, 2), "receiver balance updated successfully").to.be.equal(200)
+            expect(await contract.balanceOf(user1.address, 2), "spender balance updated failed").to.be.equal(9900)
+            expect(await contract.balanceOf(user2.address, 2), "receiver balance updated failed").to.be.equal(200)
 
         })
 
     });
 
 
-    describe("mintBatch Values - Case 02", async () => {
+    describe("Transfer from - Case 02", async () => {
 
         before(async () => {
             [deployer, user1, user2] = await ethers.getSigners();
@@ -76,6 +82,8 @@ describe("Pure Yul ERC1155 - mintBatch Function Testing", function () {
             contract = await baseContractFactory.deploy();
             const mintBatch1 = await contract.mintBatch(user1.address, [1, 2], [100, 10000], 0x0);
             await mintBatch1.wait();
+            await contract.connect(user1).setApprovalForAll(user2.address, true);
+            await contract.connect(user2).setApprovalForAll(user1.address, true);
         })
 
 
